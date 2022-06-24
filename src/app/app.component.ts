9ea13a5db9bc118a5c8b6cfb5a60494622af5623
Jeremy Dickey <47309416+JDEE30@users.noreply.github.com>
2022-06-23 20:20:09 -0500

    add bootstrap to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './ngbd-accordion-basic',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'framework-code-test-w-github-api';
 }
+
